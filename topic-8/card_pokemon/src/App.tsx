b26e6c5b6a8953ffca28805161469b906aab7617
armandonum <armandonunez404@gmail.com>
2024-07-29 14:16:24 -0400

    added styles

--- topic-8/card_pokemon/src/App.tsx
+++ topic-8/card_pokemon/src/App.tsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from 'react'
 
-import { usePokemon, getPokemon } from './hook/script';
+import { usePokemon, getPokemon,numberPkemon , getPokemonIcone} from './hook/script';
 
 function App() {
   const [count, setCount] = useState(1);
 
 
-  const pokemon= usePokemon(count);
+  const {pokemon}= usePokemon(count);
 
   const getPokemonImage = getPokemon(count);
+  const numberPke= numberPkemon(count);
 
 
+const typename= pokemon?.types.map((typeInfo: { type: { name: any; }; }) => typeInfo.type.name).join(', ');
+const PokemonType=getPokemonIcone(typename);
+
+const handleClick = () => {
+  const newPokemon = Math.floor(Math.random()*100)+1;
+       setCount (newPokemon);
+  
+    }
 
   return (
     <div
@@ -22,35 +31,65 @@ function App() {
         width: '100vw',
         height: '100vh',
         backgroundColor: 'orange',
+        font: '150% sans-serif',
+       
       }}>
 
 
       <div style={
         {
           display: 'flex',
-          border: '3px solid black',
-          background: 'red'
+         
+          background: 'red',
+          padding: '10px',
+          margin: '20px',
+          borderRadius: '2rem',
+          boxShadow: '0 0 10px rgba(0,0,0,5)',
 
         }
       }>
-        <div>
-          <div>
-            number and name
-            <h2>Count: {count} </h2>
-            <h2>{pokemon.pokemon}</h2>
+        <div
+       >
+          <div
+           style={
+            {
+          
+              
+            margin: '20px',
+            }
+          }
+          >
+       
+            <h2># {numberPke} </h2>
+            <h2>{pokemon?.name}</h2>
+            
+           
+
           </div>
 
-          <div>type</div>
+          <div> 
+            <h2>{typename}</h2>
+            {/* <h2>{pokemon?.types.map(typeInfo => typeInfo.type.name).join(', ')}</h2> */}
+           <img src={PokemonType} alt="" /></div>
 
         </div>
 
-        <div>
-          <button onClick={() => setCount((count) => count + 1)}>Increment</button>
+        <div
+         style={
+          {
+            display: 'flex',
+          
+            padding: '10px',
+          margin: '20px',
+          }
+        }>
+          
           <img src={getPokemonImage} alt="Pokemon" />
         </div>
-
+       
 
       </div>
+      <button onClick={handleClick}>Increment</button>
     </div>
   )
 }
--- topic-8/card_pokemon/src/hook/script.tsx
+++ topic-8/card_pokemon/src/hook/script.tsx
@@ -1,8 +1,9 @@
 
-import { useState,useEffect } from "react"
+import { useState,useEffect } from "react";
 
 const url = "https://pokeapi.co/api/v2/pokemon/"
 
+const typeUrl= "https://pokeapi.co/api/v2/type/"
 
 function getData( id: number)
 {
@@ -11,25 +12,46 @@ function getData( id: number)
     .then(data => data)
 }
 
-function usePokemon(id: number)
-{
-    const [pokemon, setPokemon]=useState(null);
-    useEffect (() =>
-    {
-        getData(id).then(pokemon => setPokemon(pokemon));
 
-        console.log(pokemon);
-     } ,  [id]);
-
-     return {pokemon};
+function getPokemonTypeData() {
+    return fetch(typeUrl)
+        .then(response => response.json())
+        .then(data => data);
 }
 
 
-function getPokemon(id:number)
+function usePokemon(id: number): { pokemon: any }
+{
+    const [pokemon, setPokemon] = useState(null);
+    useEffect   (() => {
+        getData(id).then(pokemon => setPokemon(pokemon));
+        console.log(pokemon);
+
+    }, [id]);
+    return {pokemon}; 
+
+
+}
+
+function getPokemon(id:any)
 {
     const realId = `00${id}`.slice(-3);
     return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${realId}.png`;
 }
 
 
-export  {usePokemon,getPokemon};
+function getPokemonIcone(id:any)
+
+{
+    const type= `${id}.svg`;
+    
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${type}`;
+}
+function numberPkemon(id:number)
+{
+    const realId = `00${id}`.slice(-3);
+    return realId;
+
+}
+
+export  {usePokemon,getPokemon, numberPkemon, getPokemonIcone, getPokemonTypeData};
